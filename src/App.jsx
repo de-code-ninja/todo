3a4b75ffef371663865aab5243b4c67fe39065dd
NexusWarp <ik571572@gmail.com>
2024-04-24 04:13:37 +0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,20 @@
 import React, { useState,useContext,useEffect } from "react";
 import "./App.css";
-import Navbar from "./Navbar";
-import Login from "./Login";
+
 import Footer from "./Footer";
-import Homepage from "./Homepage";
-import Signup from "./Signup";
-import Profile from "./Profile";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+
 import { AuthProvider } from "./context/AuthContext";
-import { AuthContext } from "./context/AuthContext";
+
 
 
 function App() {
   
-  const router = createBrowserRouter([
-    {
-      path:"/",
-      element: <><Navbar/> <Homepage/></>
-    },
-    {
-      path:"/login",
-      element: <><Navbar /> <Login/></>
-    },
-    {
-      path:"/signup",
-      element: <><Navbar/> <Signup/></>
-    },
-    {
-      path:"/profile",
-      element: <><Navbar /> <Profile/></>
-    },
-  ])
+
   return (
     <AuthProvider>
   <>
-      
-      <RouterProvider router={router} />
+      <div>hello react</div>
+    
      
       <Footer/>
     </>
