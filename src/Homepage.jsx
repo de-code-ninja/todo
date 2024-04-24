2aa5be9e6972697e4221c4550c39ac014a9de31e
de-code-ninja <166255748+de-code-ninja@users.noreply.github.com>
2024-04-24 17:51:59 +0500

    Update Homepage.jsx

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -18,10 +18,10 @@ function Homepage(props) {
         return;
       }
     // const userId = user._id;
-    console.log("Fetching data for user:", user);
+   
     const response = await fetch(`https://todobackend-suuw.onrender.com/api/get/data/${user._id}`);
     const data = await response.json();
-    console.log(data);
+   
     
     settaskArr(data);
   }catch(err){
@@ -47,7 +47,7 @@ async function submitTask(){
     body: JSON.stringify(dataToSend)
   })
   if (response.ok) {
-    console.log("Data sent successfully");
+   
     fetchData();
   } else {
     console.error("Failed to send data");
@@ -67,7 +67,7 @@ async function deleteBtn(id){
         body: JSON.stringify(sendId)
       })
       if (response.ok) {
-        console.log("Id sent successfully");
+        
         fetchData();
       } else {
         console.error("Failed to send data");
@@ -77,13 +77,13 @@ async function deleteBtn(id){
         console.log(err);
   }
 
-console.log(id);
+
 }
 
   return (
     <div className="flex items-center flex-col mt-8  drop-shadow-md selection:bg-amber-400 selection:text-black">
       <div
-        className="w-2/4 h-60 flex justify-center items-center gap-3 flex-col "
+        className="lg:w-2/4 w-full h-60 flex justify-center items-center gap-3 flex-col "
         id="textarea"
       >
         <textarea
@@ -95,29 +95,29 @@ console.log(id);
           onChange={handleChanges}
           value={task}
           placeholder="Type here..."
-          className="w-3/4 h-2/4 text-xl p-2 border-2 border-black rounded-xl"
+          className="lg:w-full xl:w-3/4 w-5/6 md:w-3/4 h-2/4 text-xl p-2 border-2 border-black rounded-xl"
         >
           
         </textarea>
-        <button onClick={submitTask} disabled={!user} className="bg-amber-400 hover:bg-amber-300 hover:text-black text-white p-3 w-3/4 rounded-xl">
+        <button onClick={submitTask} disabled={!user} className="bg-amber-400 hover:bg-amber-300 hover:text-black text-white p-3 lg:w-full xl:w-3/4 w-5/6 md:w-3/4 rounded-xl">
           Submit
         </button>
       </div>
-<div id="center" className="w-2/4 h-10 flex  mt-10  mb-5 justify-center"> <div className="w-2/4 h-full bg-black flex justify-center text-white items-center font-semibold"><h3>{user ? `Hi ${user.name}, Here is your list`:"Not signed in! Please Log in or Sign up"}</h3></div> </div>
+<div id="center" className="xl:w-2/4 lg:w-3/4 sm:w-3/4 w-full h-10 flex  mt-10  mb-5 justify-center"> <div className="lg:w-2/4 w-5/6 h-full bg-black flex justify-center text-white text-xs md:text-sm lg:text-md items-center font-semibold"><h3>{user ? `Hi ${user.name}, Here is your list`:"Not signed in! Please Log in or Sign up"}</h3></div> </div>
       <div
         id="lists-div"
-        className="w-2/4 flex flex-col items-center mb-32 gap-4 pt-5"
+        className="lg:w-2/4 w-full flex flex-col items-center mb-32 gap-4 pt-5"
       >
 
-      
+
       {taskArr.map((item,index)=>(
  
       
-         <div key={index} id="list" className="w-3/4 h-20 bg-slate-100 flex justify-between items-end">
-          <div id="circle" className="w-20 h-20 bg-amber-400 rounded-r-full flex justify-center items-center text-5xl font-sans font-bold"> <h1>{index+1}</h1></div>
-          <div id="text" className="w-3/4 h-20  flex justify-start items-center font-sans"> 
+         <div key={index} id="list" className="lg:w-full xl:w-3/4 w-5/6 md:w-3/4 h-20 bg-slate-100 flex justify-between items-end">
+          <div id="circle" className="w-10 sm:w-12 lg:w-16 xl:w-20 h-20 bg-amber-400 rounded-r-full flex justify-center items-center text-4xl lg:text-5xl font-sans font-bold"> <h1>{index+1}</h1></div>
+          <div id="text" className="sm:w-3/4 w-3/4 h-20 text-sm flex justify-start items-center font-sans"> 
           <p>{item.task}</p></div>
-          <div id="delBtn" className="w-8 h-8 bg-black flex justify-center items-center hover:cursor-pointer text-1xl font-sans hover:text-amber-400 text-white font-semibold" onClick={()=>{deleteBtn(item._id)}}> <h3>X</h3></div>
+          <div id="delBtn" className="w-6 lg:w-8 lg:h-8 h-6 bg-black flex justify-center items-center hover:cursor-pointer text-sm lg:text-xl font-sans hover:text-amber-400 text-white font-semibold" onClick={()=>{deleteBtn(item._id)}}> <h3>X</h3></div>
         </div>
     
 
