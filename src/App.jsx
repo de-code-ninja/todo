8a64e1f0fa9ec31b8a481b0c0253fd01e1f74ac5
NexusWarp <ik571572@gmail.com>
2024-04-24 15:34:15 +0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,29 @@
-import React, { useState,useContext,useEffect } from "react";
+import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Navbar";
 import Login from "./Login";
 import Footer from "./Footer";
 import Homepage from "./Homepage";
 import Signup from "./Signup";
 import Profile from "./Profile";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
-import { AuthContext } from "./context/AuthContext";
-
 
 function App() {
-  
-  const router = createBrowserRouter([
-    {
-      path:"/",
-      element: <><Navbar/> <Homepage/></>
-    },
-    {
-      path:"/login",
-      element: <><Navbar /> <Login/></>
-    },
-    {
-      path:"/signup",
-      element: <><Navbar/> <Signup/></>
-    },
-    {
-      path:"/profile",
-      element: <><Navbar /> <Profile/></>
-    },
-  ])
   return (
     <AuthProvider>
-  <>
-
-      <Homepage/>
-      
-     
-      <Footer/>
-    </>
+      <Router>
+        <div>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/profile" element={<Profile />} />
+          </Routes>
+          <Footer />
+        </div>
+      </Router>
     </AuthProvider>
   );
 }
